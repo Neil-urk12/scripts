5f5ebef880c19c250fb7907bdf3c66041f825df8
Neil Vallecer <neilvallecer12@gmail.com>
2025-10-07 08:17:55 +0800

    feat: finally committed this php monkey script

--- php-setup.sh
+++ php-setup.sh
@@ -3,8 +3,9 @@ sudo dnf install httpd -y
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --reload
-sudo dnf instll mariadb-server -y
+sudo dnf install mariadb-server -y
 sudo systemctl enable --now mariadb
 sudo mysql_secure_installation
-sudo dnf install php php-mysqlnd php-cli php-common -y
+sudo dnf install php php-mysqlnd php-cli php-common php-gd php-xml php-mbstring -y
 sudo systemctl restart httpd
+sudo systemctl enable httpd
